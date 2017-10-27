676c13d2386c76ec6134ef66f385c88216029d2d
Ausiàs <espaming@gmail.com>
2017-10-27 21:34:30 +0200

    Adds UI labels to main view controller to display sensor data from the inertial unit (roll, pitch, yaw).

--- MotionController/ViewController.swift
+++ MotionController/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var rollValue: UILabel!
+    
+    @IBOutlet weak var pitchValue: UILabel!
+    
+    @IBOutlet weak var yawValue: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
